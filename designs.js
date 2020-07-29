ce5251c293f95e8c5c72919f7de3291335ba3240
Enrico Yu <68810639+EnricoYu@users.noreply.github.com>
2020-07-30 01:55:41 +0200

    Update designs.js

--- designs.js
+++ designs.js
@@ -3,17 +3,13 @@ let pixelCanvas = document.getElementById('pixelCanvas')
 let height = document.getElementById('inputHeight')
 let width = document.getElementById('inputWidth')
 let color = document.getElementById('colorPicker')
-let clicking = false;
-
 function makeGrid() {
   for (let a = 0; a <= height.value; a++) {
     let row = pixelCanvas.insertRow(a)
     for (let b = 0; b <= width.value; b++) {
       let cell = row.insertCell(b)
-      cell.addEventListener('mouseover', function() {
-        if (clicking == true) {
+      cell.addEventListener('click', function() {
         this.setAttribute("style", `background-color: ${color.value}`)
-      }
     })
     }
   }
@@ -29,14 +25,4 @@ sizePicker.addEventListener('submit', function(refresh) {
   refresh.preventDefault()
   removeGrid()
   makeGrid()
-})
-
-pixelCanvas.addEventListener('mousedown', function() {
-	clicking = true
-})
-pixelCanvas.addEventListener('mouseup', function() {
-	clicking = false
-})
-pixelCanvas.addEventListener('mouseleave', function() {
-  clicking = false
 })
